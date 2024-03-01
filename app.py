f3492bed20abbd91861f9643ad501acde9dbec26
Emmanuel Boniface <bonifacechosen100@gmail.com>
2024-03-01 10:11:20 +0100

    features issues

--- app.py
+++ app.py
@@ -49,9 +49,8 @@ def show_prediction_interface():
     if submit_button and account_name and username:  # Ensure account name and username are provided
         input_df = pd.DataFrame([
     [profile_pic, name_eq_username, description_length, external_url, private, posts, followers, follows]
-], columns=[
-    'profile_pic', 'name_eq_username', 'description_length', 'external_url', 'private', 'posts', 'followers', 'follows'
-])
+], columns=scaler.feature_names_in_)
+
 
         # Load the pre-fitted scaler and model
         scaler = joblib.load('social_media_scaler.pkl')  # Update the path to your scaler
